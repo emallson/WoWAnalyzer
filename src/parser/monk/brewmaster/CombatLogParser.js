ac82d3c7dc71a9d16720f1463b55d9532331f203
J. David Smith <emallson@atlanis.net>
2018-12-18 12:25:52 -0500

    starting a mitigation value list

--- src/parser/monk/brewmaster/CombatLogParser.js
+++ src/parser/monk/brewmaster/CombatLogParser.js
@@ -34,6 +34,7 @@ import Abilities from './modules/Abilities';
 import AlwaysBeCasting from './modules/features/AlwaysBeCasting';
 import StaggerPoolGraph from './modules/features/StaggerPoolGraph';
 import MitigationCheck from './modules/features/MitigationCheck';
+import MitigationSheet from './modules/features/MitigationSheet';
 
 // Items
 // normalizers
@@ -61,6 +62,7 @@ class CombatLogParser extends CoreCombatLogParser {
     alwaysBeCasting: AlwaysBeCasting,
     abilities: Abilities,
     staggerPoolGraph: StaggerPoolGraph,
+    sheet: MitigationSheet,
 
     // Spells
     ironSkinBrew: IronSkinBrew,
